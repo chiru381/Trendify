e33e9e2ce823e6d04f4c7f5c6f75b7085328d7a1
chiru381 <chirukosanam123@gmail.com>
2025-03-10 20:41:42 +0530

    server file & other files

---
+++ server.js
@@ -0,0 +1,170 @@
+import http from 'http';
+import { Server } from 'socket.io';
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import path from 'path';
+import productRouter from './routes/productRouter.js';
+import userRouter from './routes/userRouter.js';
+import orderRouter from './routes/orderRouter.js';
+import uploadRouter from './routes/uploadRouter.js';
+
+import Stripe from "stripe";
+const stripe = new Stripe('sk_test_51QOx3bSBnvO09ua3ly0HRXaliBlhWVq1eFyXdIH9ehh14Rhcb3XznBEOq4DSFDQ3XCOKNabMqheEAg7TC7QoUBvq008JuKo5IC');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect('mongodb://0.0.0.0:27017/trend',  {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => console.log('MongoDB - Connected Successfully'))
+  .catch(err => console.log(err));
+
+  app.use(cors({
+    origin: 'http://localhost:3000',  // Frontend URL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }));
+
+app.use(bodyParser.json());
+
+app.use('/api/uploads', uploadRouter);
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
+app.use('/api/orders', orderRouter);
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.PAYPAL_CLIENT_ID || 'sb');
+});
+
+app.post("/api/create-payment-intent", async (req, res) => {
+  try {
+    const { amount, currency, shipping } = req.body;
+
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency,
+      description: "E-commerce product purchase",
+      shipping: {
+        name: shipping.name,
+        address: {
+          line1: shipping.address.line1,
+          city: shipping.address.city,
+          state: shipping.address.state,
+          postal_code: shipping.address.postal_code,
+          country: shipping.address.country,
+        },
+      },
+    });
+
+    res.send({ clientSecret: paymentIntent.client_secret });
+  } catch (error) {
+    res.status(400).send({ error: error.message });
+  }
+});
+
+
+app.get('/api/config/google', (req, res) => {
+  res.send(process.env.GOOGLE_API_KEY || '');
+});
+const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+// app.use(express.static(path.join(__dirname, '/frontend/build')));
+// app.get('*', (req, res) =>
+//   res.sendFile(path.join(__dirname, '/frontend/build/index.html'))
+// );
+// app.get('/', (req, res) => {
+//   res.send('Server is ready');
+// });
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
+
+const port = process.env.PORT || 8000;
+
+const httpServer = http.Server(app);
+const io = new Server(httpServer, { cors: { origin: '*' } });
+const users = [];
+
+io.on('connection', (socket) => {
+  console.log('connection', socket.id);
+  socket.on('disconnect', () => {
+    const user = users.find((x) => x.socketId === socket.id);
+    if (user) {
+      user.online = false;
+      console.log('Offline', user.name);
+      const admin = users.find((x) => x.isAdmin && x.online);
+      if (admin) {
+        io.to(admin.socketId).emit('updateUser', user);
+      }
+    }
+  });
+  socket.on('onLogin', (user) => {
+    const updatedUser = {
+      ...user,
+      online: true,
+      socketId: socket.id,
+      messages: [],
+    };
+    const existUser = users.find((x) => x._id === updatedUser._id);
+    if (existUser) {
+      existUser.socketId = socket.id;
+      existUser.online = true;
+    } else {
+      users.push(updatedUser);
+    }
+    console.log('Online', user.name);
+    const admin = users.find((x) => x.isAdmin && x.online);
+    if (admin) {
+      io.to(admin.socketId).emit('updateUser', updatedUser);
+    }
+    if (updatedUser.isAdmin) {
+      io.to(updatedUser.socketId).emit('listUsers', users);
+    }
+  });
+
+  socket.on('onUserSelected', (user) => {
+    const admin = users.find((x) => x.isAdmin && x.online);
+    if (admin) {
+      const existUser = users.find((x) => x._id === user._id);
+      io.to(admin.socketId).emit('selectUser', existUser);
+    }
+  });
+
+  socket.on('onMessage', (message) => {
+    if (message.isAdmin) {
+      const user = users.find((x) => x._id === message._id && x.online);
+      if (user) {
+        io.to(user.socketId).emit('message', message);
+        user.messages.push(message);
+      }
+    } else {
+      const admin = users.find((x) => x.isAdmin && x.online);
+      if (admin) {
+        io.to(admin.socketId).emit('message', message);
+        const user = users.find((x) => x._id === message._id && x.online);
+        user.messages.push(message);
+      } else {
+        io.to(socket.id).emit('message', {
+          name: 'Admin',
+          body: 'Sorry. I am not online right now',
+        });
+      }
+    }
+  });
+});
+
+httpServer.listen(port, () => {
+  console.log(`Serve at http://localhost:${port}`);
+});
+
+// app.listen(port, () => {
+//   console.log(`Serve at http://localhost:${port}`);
+// });
